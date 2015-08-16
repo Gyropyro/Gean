806a6e172e0fb31597847e7fcb7a3b2786095968
Sam Holder <s.holder@fidelitytechnologies.com>
2015-08-16 17:35:44 +0100

    Updated plugin for new refactoring.

--- gean-maven-plugin/src/main/java/uk/co/samholder/gean/maven/gean/maven/plugin/GeanMojo.java
+++ gean-maven-plugin/src/main/java/uk/co/samholder/gean/maven/gean/maven/plugin/GeanMojo.java
@@ -23,7 +23,7 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
-import uk.co.samholder.games.datawizard.Gean;
+import uk.co.samholder.games.gean.Gean;
 
 /**
  *
